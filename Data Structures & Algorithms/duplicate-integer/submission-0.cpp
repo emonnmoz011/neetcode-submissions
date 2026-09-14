47a64ee81792f40d3761bc5d53e90d660df10dd4
Mohammad Nooruddin <97912839+emonnmoz011@users.noreply.github.com>
2026-09-14 18:49:39 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> u_set;
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(u_set.find(nums[i]) != u_set.end()) return true;
+
+            u_set.insert(nums[i]);
+        }
+
+        return false;
+    }
+};
