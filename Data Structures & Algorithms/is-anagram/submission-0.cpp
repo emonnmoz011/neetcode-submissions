ccbebbd33a972f45f623adeb627eb49c5f028330
Mohammad Nooruddin <97912839+emonnmoz011@users.noreply.github.com>
2026-09-14 19:22:00 -0500

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        unordered_map<char, int> count1;
+        unordered_map<char, int> count2;
+
+        if(s.size() != t.size()) return false;
+
+        for(int i=0; i<s.size(); i++)
+        {
+            count1[s[i]]++;
+            count2[t[i]]++;
+        }
+
+        if(count1 == count2) return true;
+        else return false;
+
+        
+    }
+};
